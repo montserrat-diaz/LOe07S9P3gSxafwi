69abd9e21cd4ec050cd42b77b019d3a5fe655bed
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2024-01-14 17:04:13 -0600

    Update and rename 3_ranking_and_displaying_profiles.py to ranking_functions.py

--- apziva_prj_3/src/models/ranking_functions.py
+++ apziva_prj_3/src/models/ranking_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 def rank_and_display_profiles(data_concat, column_name, top_n=7):
     ranked_profiles = data_concat.sort_values(by=column_name, ascending=False).head(top_n)
     print("Top {} Profiles:".format(top_n))
