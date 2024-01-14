af3e7c4abef7f0069b4c9959e07806d249d8f29c
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2024-01-14 17:00:54 -0600

    Update and rename 1_encoding_data_and_query.py to encoding_functions.py

--- apziva_prj_3/src/models/encoding_functions.py
+++ apziva_prj_3/src/models/encoding_functions.py
@@ -1,4 +1,6 @@
 from sentence_transformers import SentenceTransformer
+
+# Function for encoding data and initial query
 def encoding_data_and_query(model_name, query_sentences):
   model = SentenceTransformer(model_name)
 
@@ -9,3 +11,9 @@ def encoding_data_and_query(model_name, query_sentences):
   query_embeddings = model.encode(query_sentences)
 
   return model, profile_embeddings, query_embeddings
+
+# Function for encoding new query
+def encoding_new_query(model, selected_profile):
+  new_query_sentence = [selected_profile]
+  new_query_embedding = model.encode(new_query_sentence)
+  return new_query_embedding
