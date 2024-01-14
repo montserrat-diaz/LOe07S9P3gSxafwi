3bea37034a76aa2206ebf2237b56558736123e5d
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2024-01-14 16:48:51 -0600

    Update and rename train_model.py to encoding_data_and_query.py

---
+++ apziva_prj_3/src/models/encoding_data_and_query.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
+def encoding_data_and_query(model_name, query_sentences):
+  model = SentenceTransformer(model_name)
+
+  # Encoding profiles
+  profile_embeddings = model.encode(data_concat['data_concat'])
+
+  # Encoding query sentences
+  query_embeddings = model.encode(query_sentences)
+
+  return model, profile_embeddings, query_embeddings
